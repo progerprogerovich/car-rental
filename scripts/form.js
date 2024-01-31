fe07339cb9ca55b4e6f638a2417382319a8fdcdb
progerprogerovich <102922913+progerprogerovich@users.noreply.github.com>
2024-01-31 22:36:49 +0300

    Доработка и валидация форм

--- scripts/form.js
+++ scripts/form.js
@@ -29,3 +29,39 @@ for (let i = 0; i < modalBg.length; i++) {
 for (let i = 0; i < buttonOrder.length; i++) {
   buttonOrder[i].onclick = () => (modalBg[0].style.display = "block");
 }
+
+function validateForm() {
+  let name = document.getElementById("surname_name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let marka = document.getElementById("marka").value;
+  let model = document.getElementById("model").value;
+  let year = document.getElementById("year").value;
+  let mileage = document.getElementById("mileage").value;
+
+  // Простая проверка наличия значений
+  if (
+    name.trim() === "" ||
+    email.trim() === "" ||
+    phone.trim() === "" ||
+    marka.trim() === "" ||
+    model.trim() === "" ||
+    year.trim() === "" ||
+    mileage.trim() === ""
+  ) {
+    alert("Заполните все обязательные поля");
+    return false;
+  }
+
+  // Проверка формата email с использованием регулярного выражения
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("Введите корректный email");
+    return false;
+  }
+
+  // Дополнительные проверки, если необходимо
+
+  // Если все проверки прошли успешно, можно отправить форму
+  return true;
+}
